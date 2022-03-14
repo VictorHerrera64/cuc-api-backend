107bb649b9e102360d82fda9eaefda9ab0254f63
HERRERA AYOS VICTOR <vherrera8@cuc.edu.co>
2022-03-14 07:42:26 -0500

    se agrego cliente controler

--- src/clientes/clientes.controller.ts
+++ src/clientes/clientes.controller.ts
@@ -5,34 +5,34 @@ import { UpdateClienteDto } from './dto/update-cliente.dto';
 
 @Controller('clientes')
 export class ClientesController {
-  constructor(private readonly clientesService: ClientesService) {}
+  constructor(private readonly clientesService: ClientesService) { }
 
   @Post()
   create(@Body() createClienteDto: CreateClienteDto) {
     return "Aqui se crea un Nuevo cliente"
-   
+
   }
 
   @Get()
   findAll() {
     return "Devuelve la info del cliente de todos los clientes";
-    
+
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return "Devuelve la info del cliente # "+id;
-  
+    return "Devuelve la info del cliente # " + id;
+
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateClienteDto: UpdateClienteDto) {
-    return "Actualizamos la info del cliente # "+id;
- 
+    return "Actualizamos la info del cliente # " + id;
+
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return "Eliminamos cliente # "+id
+    return "Eliminamos cliente # " + id
   }
 }
