c8af6cf08cbfb95ab086f5922cca3e9d42635895
HERRERA AYOS VICTOR <vherrera8@cuc.edu.co>
2022-03-14 09:08:37 -0500

    se agrego base de datos pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientesModule } from './clientes/clientes.module';
 import { ProductosModule } from './productos/productos.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [ClientesModule, ProductosModule],
+  imports: [ClientesModule, ProductosModule,
+      TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'cucapi',
+      schema: 'public',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
